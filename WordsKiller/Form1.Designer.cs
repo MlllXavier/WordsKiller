846452d7e2e4ad7b4a09a35a4c9d2eb0f1535411
MlllXavier <13008113656@163.com>
2021-03-27 12:23:14 +0800

    first commit

---
+++ WordsKiller/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WordsKiller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnlOption = new System.Windows.Forms.Panel();
+            this.lblD = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.axWindowsMediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.pnlOption.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlOption
+            // 
+            this.pnlOption.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.pnlOption.Controls.Add(this.lblD);
+            this.pnlOption.Controls.Add(this.lblC);
+            this.pnlOption.Controls.Add(this.lblB);
+            this.pnlOption.Controls.Add(this.lblA);
+            this.pnlOption.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlOption.Location = new System.Drawing.Point(0, 980);
+            this.pnlOption.Name = "pnlOption";
+            this.pnlOption.Size = new System.Drawing.Size(1920, 100);
+            this.pnlOption.TabIndex = 12;
+            this.pnlOption.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlOption_Paint);
+            // 
+            // lblD
+            // 
+            this.lblD.BackColor = System.Drawing.Color.Transparent;
+            this.lblD.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblD.ForeColor = System.Drawing.Color.White;
+            this.lblD.Location = new System.Drawing.Point(1440, 0);
+            this.lblD.Name = "lblD";
+            this.lblD.Size = new System.Drawing.Size(480, 100);
+            this.lblD.TabIndex = 3;
+            this.lblD.Tag = "4";
+            this.lblD.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblD.Visible = false;
+            this.lblD.Click += new System.EventHandler(this.lblA_Click);
+            this.lblD.MouseEnter += new System.EventHandler(this.lblA_MouseEnter);
+            this.lblD.MouseLeave += new System.EventHandler(this.lblA_MouseLeave);
+            // 
+            // lblC
+            // 
+            this.lblC.BackColor = System.Drawing.Color.Transparent;
+            this.lblC.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblC.ForeColor = System.Drawing.Color.White;
+            this.lblC.Location = new System.Drawing.Point(960, 0);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(480, 100);
+            this.lblC.TabIndex = 2;
+            this.lblC.Tag = "3";
+            this.lblC.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblC.Visible = false;
+            this.lblC.Click += new System.EventHandler(this.lblA_Click);
+            this.lblC.MouseEnter += new System.EventHandler(this.lblA_MouseEnter);
+            this.lblC.MouseLeave += new System.EventHandler(this.lblA_MouseLeave);
+            // 
+            // lblB
+            // 
+            this.lblB.BackColor = System.Drawing.Color.Transparent;
+            this.lblB.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblB.ForeColor = System.Drawing.Color.White;
+            this.lblB.Location = new System.Drawing.Point(480, 0);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(480, 100);
+            this.lblB.TabIndex = 1;
+            this.lblB.Tag = "2";
+            this.lblB.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblB.Visible = false;
+            this.lblB.Click += new System.EventHandler(this.lblA_Click);
+            this.lblB.MouseEnter += new System.EventHandler(this.lblA_MouseEnter);
+            this.lblB.MouseLeave += new System.EventHandler(this.lblA_MouseLeave);
+            // 
+            // lblA
+            // 
+            this.lblA.BackColor = System.Drawing.Color.Transparent;
+            this.lblA.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblA.ForeColor = System.Drawing.Color.White;
+            this.lblA.Location = new System.Drawing.Point(0, 0);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(480, 100);
+            this.lblA.TabIndex = 0;
+            this.lblA.Tag = "1";
+            this.lblA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblA.Visible = false;
+            this.lblA.Click += new System.EventHandler(this.lblA_Click);
+            this.lblA.MouseEnter += new System.EventHandler(this.lblA_MouseEnter);
+            this.lblA.MouseLeave += new System.EventHandler(this.lblA_MouseLeave);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // axWindowsMediaPlayer
+            // 
+            this.axWindowsMediaPlayer.Enabled = true;
+            this.axWindowsMediaPlayer.Location = new System.Drawing.Point(145, 117);
+            this.axWindowsMediaPlayer.Name = "axWindowsMediaPlayer";
+            this.axWindowsMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer.OcxState")));
+            this.axWindowsMediaPlayer.Size = new System.Drawing.Size(75, 23);
+            this.axWindowsMediaPlayer.TabIndex = 13;
+            this.axWindowsMediaPlayer.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Controls.Add(this.axWindowsMediaPlayer);
+            this.Controls.Add(this.pnlOption);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlOption.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlOption;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblD;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Timer timer;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer;
+    }
+}
+
---
+++ WordsKiller/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Drawing2D;
+using System.Net;
+
+namespace WordsKiller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int CS_NOCLOSE = 0x200;
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle = cp.ClassStyle | CS_NOCLOSE;
+                return cp;
+            }
+        }
+
+        string[] oriwords;//存所有的单词
+        string[][] words = new string[12][];//存12个单词
+        List<Label> listLable = new List<Label>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Properties.Resources.background;
+            //打开单词文件
+            FileStream fs = new FileStream("words.txt",FileMode.Open);
+            StreamReader sr = new StreamReader(fs, Encoding.Default);
+            string ori = sr.ReadToEnd().Replace("\r", "");
+            oriwords = ori.Split('\n');
+            //生成12个随机的单词
+            Random random = new Random();
+            for (int i = 0; i < 12; i++)
+            {
+                int index = random.Next(0, oriwords.Length - 1);
+                words[i] = oriwords[index].Split(' ');
+            }
+
+            string http = "http://dict.youdao.com/speech?audio=";
+            for (int i = 0; i < 12; i++)
+            {
+                HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(http + words[i][0]);
+                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+                Stream st = resp.GetResponseStream();
+                Stream so = new FileStream(i + ".mp3", FileMode.Create);
+
+                long totalDownlodeByte = 0;
+                byte[] by = new byte[1024];
+                int oSize = st.Read(by, 0, by.Length);
+                while (oSize > 0)
+                {
+                    totalDownlodeByte += oSize;
+                    Application.DoEvents();
+                    so.Write(by, 0, oSize);
+
+                    oSize = st.Read(by, 0, by.Length);
+                }
+                so.Flush();
+                so.Close();
+                st.Close();
+            }
+            
+            //生成12个label
+            int k = 0;
+            for(int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Label label = new Label();
+                    label.Size = new Size(455, 300);
+                    label.Location = new Point(20 + j * 475, 20 + i * 320);
+                    label.BackColor = Color.FromArgb(36, 255, 255, 255);
+                    label.Font = new Font("微软雅黑", 28);
+                    label.ForeColor = Color.White;
+                    label.Image = Properties.Resources._lock;
+                    label.TextAlign = ContentAlignment.MiddleCenter;
+                    label.Visible = true;
+                    label.Tag = k++;
+                    label.Click += lbl_Click;
+                    label.MouseEnter += lbl_MouseEnter;
+                    label.MouseLeave += lbl_MouseLeave;
+                    this.Controls.Add(label);
+                    listLable.Add(label);
+                }
+            }
+        }
+
+        int correct = 0;
+        private void paintOption()//随机生成选项
+        {
+            Random random = new Random();
+            string A = oriwords[random.Next(0, oriwords.Length - 1)].Split(' ')[1];
+            string B = oriwords[random.Next(0, oriwords.Length - 1)].Split(' ')[1];
+            string C = oriwords[random.Next(0, oriwords.Length - 1)].Split(' ')[1];
+            string D = oriwords[random.Next(0, oriwords.Length - 1)].Split(' ')[1];
+
+            lblA.Text = "A. " + A;
+            lblB.Text = "B. " + B;
+            lblC.Text = "C. " + C;
+            lblD.Text = "D. " + D;
+
+            correct = random.Next(1, 4);
+            string correctword = words[presentWord][1];
+            switch (correct)
+            {
+                case 1:
+                    lblA.Text = "A. " + correctword;
+                    break;
+                case 2:
+                    lblB.Text = "B. " + correctword;
+                    break;
+                case 3:
+                    lblC.Text = "C. " + correctword;
+                    break;
+                case 4:
+                    lblD.Text = "D. " + correctword;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void pnlOption_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            if (presentWord == -1)
+            {
+                g.DrawString("点击任意一张单词卡", new Font("微软雅黑", 28), new SolidBrush(Color.White), new Point(780, 20));
+            }
+        }
+
+        bool isClick = false;
+
+        private void lblA_Click(object sender, EventArgs e)
+        {
+            if (true == isClick)
+            {
+                isClick = false;
+                Label label = (Label)sender;
+                int index = int.Parse((string)label.Tag);
+                if (index == correct)
+                {
+                    label.Text = "正确";
+                    locker[presentWord] = 2;
+                    presentLabel.Text = words[presentWord][0] + "\n" + words[presentWord][1];
+                }
+                else
+                {
+                    label.Text = "错误";
+                    locker[presentWord] = 3;
+                    presentLabel.Text = "10";
+                }
+            }
+        }
+
+        int count = 0;
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                if (locker[i] == 3)
+                {
+                    listLable[i].Text = (int.Parse(listLable[i].Text) - 1) + "";
+                    if (int.Parse(listLable[i].Text) == 0)
+                    {
+                        listLable[i].Text = "";
+                        listLable[i].Image = Properties.Resources._lock;
+                        locker[i] = 0;
+                    }
+                }
+                else if (locker[i] == 2)
+                {
+                    count++;
+                    if (count == 12)
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+            }
+            count = 0;
+        }
+
+        private void lblA_MouseEnter(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            label.BackColor = Color.FromArgb(36, 255, 255, 255);
+        }
+
+        private void lblA_MouseLeave(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            label.BackColor = Color.FromArgb(0, 255, 255, 255);
+        }
+
+        int[] locker = new int[12];//0：锁定，1：正在处理，2：正确，3：倒计时
+        int presentWord = -1;
+        Label presentLabel = new Label();
+
+        private void lbl_Click(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            int index = (int)label.Tag;//所点击的label的索引
+
+            if (1 == locker[index] || 2 == locker[index])
+            {
+                axWindowsMediaPlayer.URL = index + ".mp3";
+                axWindowsMediaPlayer.Ctlcontrols.play();
+            }
+            if (presentWord != -1 && locker[presentWord] == 1)//如果正在处理的单词没有选择答案
+            {
+                return;
+            }
+            if (0 == locker[index])//如果当前label是锁着的
+            {
+                isClick = true;
+                presentLabel = label;
+                presentWord = index;
+                locker[index] = 1;
+                label.Image = null;
+                label.Text = words[index][0];
+                pnlOption.Invalidate();
+                paintOption();
+                lblA.Show();
+                lblB.Show();
+                lblC.Show();
+                lblD.Show();
+                axWindowsMediaPlayer.URL = index + ".mp3";
+                axWindowsMediaPlayer.Ctlcontrols.play();
+            }
+        }
+
+        private void lbl_MouseEnter(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            label.BackColor = Color.FromArgb(72, 255, 255, 255);
+        }
+
+        private void lbl_MouseLeave(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            label.BackColor = Color.FromArgb(36, 255, 255, 255);
+        }
+    }
+}
